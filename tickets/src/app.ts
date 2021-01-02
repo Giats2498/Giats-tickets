8bc5b88ef73846a7d93c0d34a960bc973877f46a
Giats2498 <vaggelisgia@hotmail.com>
2021-01-02 23:47:39 +0200

    Fixed some Digital Ocean bugs

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -14,7 +14,10 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    //for local
+    //secure: process.env.NODE_ENV !== 'test'
+    //for prod
+    secure: false
   })
 );
 app.use(currentUser);
