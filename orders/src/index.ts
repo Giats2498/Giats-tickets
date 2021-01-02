1599020dff4f5dbe9f6e82e966bb08af92be106f
Giats2498 <vaggelisgia@hotmail.com>
2021-01-02 23:17:35 +0200

    Testing depl workflow files

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,6 +6,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
+  console.log("Starting ...");
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
